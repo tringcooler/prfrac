7bcf9b86a5534e3d6f417f8a2ed49a219c1a1138
tringcooler <tringcooler@users.noreply.github.com>
2018-03-15 00:49:23 +0800

    standard commit

--- baker.py
+++ baker.py
@@ -32,7 +32,8 @@ r2s = _range2slice()
 def rand_delt(s, pr = 0.001):
     sign = np.random.randint(2)
     delt = np.random.random() * pr / 2
-    return s + (sign * 2 - 1) * delt
+    r = s + (sign * 2 - 1) * delt
+    return min(max(r, 0), 1)
 v_rand_delt = np.vectorize(rand_delt)
 
 class _range_src(object):
@@ -93,39 +94,69 @@ def test2(src_slc, dst_slc, n = 10):
         r = baker_map(r)
         f = range_filter(r)[dst_slc]
         info.append([
-            r, f, s[f], r[f], np.sum(f),
+            r, f,
         ])
     return s, info
 
-def plot_history(src, n):
-    n += 1
+def _fit_shape(n, sc):
+    x = np.sqrt(n * sc)
+    y = np.sqrt(n / sc)
+    ymin = max(np.floor(y), 1.)
+    xmax = np.ceil((ymin + 1) * sc)
+    yr = ymin
+    while True:
+        xr = np.ceil(n / yr)
+        if xr <= xmax:
+            break
+        yr += 1
+    return int(xr), int(yr)
+
+def plots(n):
     sc = 4./3.
-    ny = np.floor(np.sqrt(n * sc))
-    nx = np.floor(np.ceil(n / ny))
-    #f, sbplts = plt.subplots(int(ny), int(nx), sharex='col', sharey='row')
-    f, sbplts = plt.subplots(int(ny), int(nx), sharex=True, sharey=True)
-    sbplts = np.array(sbplts).reshape(-1)
-    print sbplts.shape
+    nx, ny = _fit_shape(n, sc)
+    #f, sbplts = plt.subplots(ny, nx, sharex='col', sharey='row')
+    f, sbplts = plt.subplots(ny, nx, sharex=True, sharey=True)
+    return np.array(sbplts).reshape(-1)[:n]
+
+def plot_history(src, sbplts, color = 'b'):
+    n = len(sbplts)
     def _plt(s, i):
-        sbplts[i].plot(s[:,0], s[:,1], 'b.')
+        sbplts[i].plot(s[:,0], s[:,1], color + '.')
     dst = src
     _plt(dst, 0)
     for i in xrange(1, n):
         dst = baker_map(dst)
         _plt(dst, i)
-    plt.show()
+
+def plot_histories(src, dsts, sbplts):
+    n = len(sbplts) - 1
+    colors = ['b', 'r', 'g', 'y']
+    pn = min(max(len(colors), 0), n)
+    color = lambda i: colors[n - i]
+    def _plt(s, i, c):
+        sbplts[i].plot(s[:,0], s[:,1], c + '.')
+    for i in xrange(n - pn, n):
+        flt = dsts[i][1]
+        c = color(i + 1)
+        _plt(src[flt], 0, c)
+        for j in xrange(0, n):
+            dst = dsts[j][0]
+            _plt(dst[flt], j + 1, c)
 
 if __name__ == '__main__':
     pr = .0001
     nm = 20
     src_slc = r2s[.78:.83:pr, .14:.19:pr]
     dst_slc = r2s[.68:.73, .24:.29]
-    s, r, ds, dr = test(src_slc, dst_slc, nm)
+    #s, r, ds, dr = test(src_slc, dst_slc, nm)
     #idr1 = baker_map(dr, 1, inv = True)
     #plt.plot(ds[:,0], ds[:,1], 'r.')
     #plt.plot(dr[:,0], dr[:,1], 'b.')
     #plt.plot(idr1[:,0], idr1[:,1], 'g.')
     #plt.axis('equal')
     #plt.show()
-    plot_history(ds, nm)
+    s, rs = test2(src_slc, dst_slc, nm)
+    sbplts = plots(nm + 1)
+    plot_histories(s, rs, sbplts)
+    plt.show()
 
